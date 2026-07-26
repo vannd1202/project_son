4d776032922ce518d85c919b0b1ecf0b8c911825
BeeCon21 <nhatson0201@gmail.com>
2026-07-26 10:15:57 +0700

    Đẩy bài Thiết kế phần mềm

--- Sales_MNGT/src/dao/ProductDAO.java
+++ Sales_MNGT/src/dao/ProductDAO.java
@@ -11,25 +11,29 @@ public class ProductDAO {
     public List<Product> getAllProducts() {
         List<Product> products = new ArrayList<>();
         String sql = "SELECT * FROM Product";
-        try {
-            Connection conn = DBConnection.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
+
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+                ) {
             while(rs.next()) {
                 Product product = new Product();
+
                 product.setProductID(rs.getInt("productID"));
                 product.setName(rs.getString("name"));
                 product.setPrice(rs.getInt("price"));
                 product.setDescription(rs.getString("description"));
-                products.add(product);
-            }
-            rs.close();
-            ps.close();
-            conn.close();
 
-        } catch(Exception e) {
+                products.add(product);
+        }
+
+        } catch(SQLException e) {
             e.printStackTrace();
         }
         return products;
     }
+
+    //2. Xem chi tiết sản phẩm
+
 }
